3a9227adaf9c7cb722aa69f926ce3e708b11baa9
Jonne Junno <jonne.junno@gmail.com>
2018-11-09 16:48:29 +0200

    Fixed magicnumber for codacy

--- RiippuvuuksienInjektointi1/src/main/java/ohtu/Laskin.java
+++ RiippuvuuksienInjektointi1/src/main/java/ohtu/Laskin.java
@@ -9,17 +9,18 @@ public class Laskin {
     public Laskin() {
         lukija = new Scanner(System.in);
     }
-    
+
     public void suorita(){
+        int endLoop = -9999;
         while( true ) {
             System.out.println("luku 1: ");
             int luku1 = lukija.nextInt();
-            if ( luku1==-9999  ) return;
-            
+            if ( luku1==endLoop  ) return;
+
             System.out.println("luku 2: ");
             int luku2 = lukija.nextInt();
-            if ( luku2==-9999  ) return;
-            
+            if ( luku2==endLoop  ) return;
+
             int vastaus = laskeSumma(luku1, luku2);
             System.out.println("summa: "+ vastaus);
         }
@@ -27,7 +28,6 @@ public class Laskin {
 
     private int laskeSumma(int luku1, int luku2) {
         return luku1+luku2;
-    }    
-   
-}
+    }
 
+}
